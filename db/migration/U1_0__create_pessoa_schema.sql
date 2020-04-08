670a40492612073a711756d17e3c8a69280bb7c8
Francisco Junior <fjuniordourado@gmail.com>
2020-04-08 09:05:27 -0300

    V1.1 - alterando nome do arquivo de migracao de U(Undo) para V(Versionadmento)

--- db/migration/U1_0__create_pessoa_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE pessoa (
-    id INT NOT NULL PRIMARY KEY,
-    nome VARCHAR NOT NULL,
-);
-
-
-INSERT INTO pessoa(id,nome) VALUES (1,'Joao Carlos');
---
+++ db/migration/V1_0__create_pessoa_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pessoa (
+  id INT NOT NULL PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL
+);
+INSERT INTO
+  pessoa(id, nome)
+VALUES
+  (1, 'Joao Carlos');
